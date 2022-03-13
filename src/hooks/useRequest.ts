0cc788ab43d10d053dde1ca7714cd882e7b1656d
arturabeltins <abeltinsh.a@icloud.com>
2022-03-13 12:18:51 +0300

    disable auto-revalidation data

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,8 +1,10 @@
 import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import { Key, SWRResponse } from 'swr/dist/types'
 
 export const useRequest = <Data = any, Error = any, SWRKey extends Key = null>(
-  key: SWRKey
+  key: SWRKey,
+  revalidate = false
 ): SWRResponse<Data, Error> => {
-  return useSWR<Data>(key)
+  return revalidate ? useSWR<Data>(key) : useSWRImmutable<Data>(key)
 }

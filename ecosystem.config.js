324b6fd9c8b2820e860255836c754ae51d291dc6
arturabeltins <abeltinsh.a@icloud.com>
2022-07-25 21:13:29 +0300

    app pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'checkpoint',
-      script: 'yarn',
-      args: "serve"
+      name: 'checkpoint-client',
+      script: 'nodemon',
+      args: '--ignore server/data -O \'{"module": "commonjs"}\' server/index.ts',
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
   ]
 }

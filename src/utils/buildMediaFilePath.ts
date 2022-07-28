ebcb0cc5df2b3a6d8b9c7799e323b84b972c42a5
arturabeltins <abeltinsh.a@icloud.com>
2022-07-28 18:12:55 +0300

    Resolve static files

--- src/utils/buildMediaFilePath.ts
+++ src/utils/buildMediaFilePath.ts
@@ -1,8 +1,6 @@
 import { MediaFile } from 'src/interfaces/MediaFile'
 
 export const buildMediaFilePath = (file: MediaFile) => {
-  const staticPath = import.meta.env.VITE_STATIC_PATH?.toString() || ''
-  const staticFolder = import.meta.env.VITE_STATIC_FOLDER?.toString() || ''
 
-  return `${staticPath}${file?.path.split(staticFolder)[1]}/${file.fileName}`
+  return `${file.path}/${file.fileName}`
 }
